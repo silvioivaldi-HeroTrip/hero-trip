b22cf7f111e675ff603e141c88bc63d57ef0901e
Silvio Ivaldi <silvio.ivaldi@gmail.com>
2025-12-15 11:31:01 +0100

    modifica finale Home Page

--- components/WeekendCard.tsx
+++ components/WeekendCard.tsx
@@ -1,34 +1,90 @@
 import Image from "next/image";
 
-export default function WeekendCard({ data, onImage }: any) {
-  const main = data.images.find((i: any) => i.main);
+type ImageItem = {
+  file: string;
+  title: string;
+  badge?: string;
+  main?: boolean;
+};
+
+type Props = {
+  data: {
+    id: string;
+    label: string;
+    analyzed: number;
+    selected: number;
+    wins: number;
+    losses: number;
+    roi: number;
+    images: ImageItem[];
+  };
+  onImage: (src: string) => void;
+};
+
+export default function WeekendCard({ data, onImage }: Props) {
+  const mainImage =
+    data.images.find((i) => i.main) || data.images[0];
+
+  const otherImages = data.images.filter(
+    (i) => i.file !== mainImage.file
+  );
+
+  const basePath = `/images/weekends/${data.id}`;
 
   return (
-    <div className="min-w-[340px] bg-[#111936] rounded-xl p-5 border border-slate-800 space-y-4">
-      <h3 className="text-amber-300 font-semibold">{data.label}</h3>
+    <div className="min-w-[360px] bg-[#111936] rounded-xl p-5 border border-slate-800 space-y-4">
+      {/* Header */}
+      <h3 className="text-amber-300 font-semibold">
+        {data.label}
+      </h3>
 
-      <ul className="text-sm text-slate-300 space-y-1">
+      <ul className="text-xs text-slate-300 space-y-1">
         <li>Analizzate: {data.analyzed}</li>
         <li>Selezionate: {data.selected}</li>
         <li>Vinte: {data.wins} | Perse: {data.losses}</li>
-        <li className="text-green-400 font-semibold">ROI: +{data.roi}%</li>
+        <li className="text-green-400 font-semibold">
+          ROI: +{data.roi}%
+        </li>
       </ul>
 
+      {/* Main image */}
       <div className="relative">
-        {main.badge && (
-          <span className="absolute top-2 left-2 bg-amber-400 text-black text-xs px-2 py-1 rounded">
-            {main.badge}
+        {mainImage.badge && (
+          <span className="absolute top-2 left-2 z-10 bg-amber-400 text-black text-xs px-2 py-1 rounded">
+            {mainImage.badge}
           </span>
         )}
+
         <Image
-          src={`/images/weekends/${data.id}/${main.file}`}
-          alt={main.title}
+          src={`${basePath}/${mainImage.file}`}
+          alt={mainImage.title}
           width={320}
           height={200}
           className="rounded-lg cursor-pointer"
-          onClick={() => onImage(`/images/weekends/${data.id}/${main.file}`)}
+          onClick={() =>
+            onImage(`${basePath}/${mainImage.file}`)
+          }
         />
       </div>
+
+      {/* Gallery */}
+      {otherImages.length > 0 && (
+        <div className="flex gap-2 overflow-x-auto pt-2">
+          {otherImages.map((img, idx) => (
+            <Image
+              key={idx}
+              src={`${basePath}/${img.file}`}
+              alt={img.title}
+              width={90}
+              height={60}
+              className="rounded cursor-pointer opacity-80 hover:opacity-100 transition"
+              onClick={() =>
+                onImage(`${basePath}/${img.file}`)
+              }
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
