a77e8f7bf2977d80a92a9f89dbacbbbe27ce0fa4
Silvio Ivaldi <silvio.ivaldi@gmail.com>
2025-12-16 15:55:29 +0100

    modifica finale route e acessi

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,14 +3,12 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
-  // 👉 credenziali base (per ora)
   if (
-    email === process.env.ADMIN_EMAIL &&
-    password === process.env.ADMIN_PASSWORD
+    email === process.env.EMAIL_USER &&
+    password === process.env.EMAIL_PASS
   ) {
     const res = NextResponse.json({ success: true });
 
-    // 🔑 COOKIE DI SESSIONE
     res.cookies.set("auth", "true", {
       httpOnly: true,
       path: "/",
