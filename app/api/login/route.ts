0972eddfc28c8836d41f80a53b1abb24d5fc0bee
Silvio Ivaldi <silvio.ivaldi@gmail.com>
2025-12-16 16:00:08 +0100

    modifica finale route e acessi

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,6 +3,11 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const { email, password } = await req.json();
 
+  console.log("EMAIL INSERITA:", email);
+  console.log("PASS INSERITA:", password);
+  console.log("ENV EMAIL:", process.env.EMAIL_USER);
+  console.log("ENV PASS:", process.env.EMAIL_PASS);
+
   if (
     email === process.env.EMAIL_USER &&
     password === process.env.EMAIL_PASS
@@ -18,8 +23,5 @@ export async function POST(req: Request) {
     return res;
   }
 
-  return NextResponse.json(
-    { success: false },
-    { status: 401 }
-  );
+  return NextResponse.json({ success: false }, { status: 401 });
 }
